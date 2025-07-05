81f06ce9b120a96127aefd33645d3620473356d0
rishabh <rishabhguptalahar@gmail.com>
2025-07-05 21:17:16 +0530

    fix import bugs

--- Frontend/src/redux/slices/API/activity.apiSlice.js
+++ Frontend/src/redux/slices/API/activity.apiSlice.js
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { get } from "mongoose";
 
 export const activityApiSlice = createApi({
   reducerPath: "activityApi",
